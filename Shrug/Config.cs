d7c8145c3aa1a572d3c296df9433b9c2bac0bb0a
Wade <67872426+kzwade@users.noreply.github.com>
2021-03-17 16:40:45 -0400

    Add files via upload

---
+++ Shrug/Config.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace ShrugPlugin
+{
+    public class Config
+    {
+        [JsonProperty("Shrug Cooldown")]
+        public static int ShrugCooldown { get; set; } = 30;
+        public static string[] RankList { get; set; } = { "User" };
+        [JsonIgnore]
+        private static readonly string Path = System.IO.Path.Combine(TShock.SavePath, "ShrugPlugin.json");
+        public static Config Create()
+        {
+            if (File.Exists(Path))
+                return Read();
+            TShock.Log.Info("Creating new Config file");
+            try
+            {
+                var conf = new Config();
+                File.WriteAllText(Path, JsonConvert.SerializeObject(conf, Formatting.Indented));
+                return conf;
+
+            }
+            catch
+            {
+                TShock.Log.ConsoleError("[ShrugPlugin] Failed to create new config file!");
+            }
+            return new Config();
+        }
+        public static Config Read(bool returnNull = false)
+        {
+            TShock.Log.Info("Reading config file");
+            try
+            {
+                var sr = new StreamReader(File.Open(Path, FileMode.Open));
+                var rawJson = sr.ReadToEnd();
+                sr.Close();
+
+                var conf = JsonConvert.DeserializeObject<Config>(rawJson);
+                File.WriteAllText(Path, JsonConvert.SerializeObject(conf, Formatting.Indented));
+                return conf;
+
+
+
+
+            }
+            catch (JsonReaderException e)
+            {
+                TShock.Log.Error(e.ToString());
+                var additional = returnNull ? "" : " Using Defaults!";
+                TShock.Log.ConsoleError($"[ShrugPlugin] Failed to load config!");
+                return returnNull ? null : new Config();
+
+            }
+        }
+
+
+
+        public class ConfigColor
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+            public Color CovertToColor() => new Color(R, G, B, 255);
+
+        }
+
+        public class OldConfig
+        {
+            [JsonProperty("ShrugPlugin")]
+            public string[] RankList { get; set; }
+
+        }
+    }
+}
---
+++ Shrug/Plugin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using TShockAPI;
+using TerrariaApi.Server;
+using Terraria;
+using Newtonsoft.Json;
+using Microsoft.Xna.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using TShockAPI.Hooks;
+
+
+namespace ShrugPlugin.cs
+
+
+{
+    [ApiVersion(2, 1)]
+
+
+
+    public class ShrugPlugin : TerrariaPlugin
+    {
+
+        public ShrugPlugin(Main game) : base(game) { }
+
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("shrugcommand.shrug", Shrug, "shrug"));
+            
+        }
+
+        private void Shrug(CommandArgs args)
+        {
+            string playerName = args.Parameters[0];
+
+            var players = TSPlayer.FindByNameOrID(playerName);
+
+            if (players.Count > 1)
+            {
+                args.Player.SendErrorMessage("Invalid player!");
+            }
+
+            if (players.Count > 1)
+            {
+                args.Player.SendMultipleMatchError(players.Select(p => p.Name));
+            }
+
+            else
+            {
+
+                TSPlayer.All.SendSuccessMessage($"{args.Player.Name} Shrugged.");
+            }
+        }
+
+    }
+}
