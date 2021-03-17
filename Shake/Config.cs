d0fd5c957c2d627476cc62f8dd1c8080f8bf19be
Wade <67872426+kzwade@users.noreply.github.com>
2021-03-17 16:35:15 -0400

    Add files via upload

---
+++ Shake/Config.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace ShakePlugin
+{
+    public class Config
+    {
+        [JsonProperty("Shake Cooldown")]
+        public static int ShakeCooldown { get; set; } = 30;
+        public static string[] RankList { get; set; } = { "User" };
+        [JsonIgnore]
+        private static readonly string Path = System.IO.Path.Combine(TShock.SavePath, "ShakePlugin.json");
+        public static Config Create()
+        {
+            if (File.Exists(Path))
+                return Read();
+            TShock.Log.Info("Creating new Config file");
+            try
+            {
+                var conf = new Config();
+                File.WriteAllText(Path, JsonConvert.SerializeObject(conf, Formatting.Indented));
+                return conf;
+
+            }
+            catch
+            {
+                TShock.Log.ConsoleError("[ShakePlugin] Failed to create new config file!");
+            }
+            return new Config();
+        }
+        public static Config Read(bool returnNull = false)
+        {
+            TShock.Log.Info("Reading config file");
+            try
+            {
+                var sr = new StreamReader(File.Open(Path, FileMode.Open));
+                var rawJson = sr.ReadToEnd();
+                sr.Close();
+
+                var conf = JsonConvert.DeserializeObject<Config>(rawJson);
+                File.WriteAllText(Path, JsonConvert.SerializeObject(conf, Formatting.Indented));
+                return conf;
+
+
+
+
+            }
+            catch (JsonReaderException e)
+            {
+                TShock.Log.Error(e.ToString());
+                var additional = returnNull ? "" : " Using Defaults!";
+                TShock.Log.ConsoleError($"[ShakePlugin] Failed to load config!");
+                return returnNull ? null : new Config();
+
+            }
+        }
+
+
+
+        public class ConfigColor
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+            public Color CovertToColor() => new Color(R, G, B, 255);
+
+        }
+
+        public class OldConfig
+        {
+            [JsonProperty("ShakePlugin")]
+            public string[] RankList { get; set; }
+
+        }
+    }
+}
---
+++ Shake/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using TShockAPI;
+using TerrariaApi.Server;
+using Terraria;
+using Newtonsoft.Json;
+using Microsoft.Xna.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using TShockAPI.Hooks;
+
+
+namespace ShakePlugin.cs
+
+
+{
+    [ApiVersion(2, 1)]
+
+
+    public class ShakePlugin : TerrariaPlugin
+    {
+
+        public ShakePlugin(Main game) : base(game) { }
+
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("shakecommand.shake", Shake, "shake"));
+          
+        }
+
+        private void Shake(CommandArgs args)
+        {
+            string playerName = args.Parameters[0];
+
+            var players = TSPlayer.FindByNameOrID(playerName);
+
+            if (players.Count > 1)
+            {
+                args.Player.SendErrorMessage("Invalid player!");
+            }
+
+            if (players.Count > 1)
+            {
+                args.Player.SendMultipleMatchError(players.Select(p => p.Name));
+            }
+
+            else
+            {
+
+                args.Player.SendInfoMessage($"You shook hands with {players[0].Name}.");
+
+                TSPlayer.All.SendSuccessMessage($"{args.Player.Name} shook hands with {players[0].Name}!");
+            }
+        }
+
+    }
+}
